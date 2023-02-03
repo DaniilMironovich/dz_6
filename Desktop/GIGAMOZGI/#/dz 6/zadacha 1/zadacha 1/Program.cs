07faedc83cd9fc6cc000c688941631450cf52c44
Danik <l13m@mail.ru>
2023-02-03 16:54:37 +0300

    pervaya zadacha

---
+++ Desktop/GIGAMOZGI/#/dz 6/zadacha 1/zadacha 1/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 4
+
+
+using System.Reflection.Metadata.Ecma335;
+
+Console.Clear();
+void PrintArr(int[] array)
+{
+    Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+int[] GenerateArr(int m)
+{
+    int[] arrNum = new int[m];
+    for (int i = 0; i < arrNum.Length; i++)
+    {
+        arrNum[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arrNum;
+}
+int CountNum(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            result += array[i];
+    }
+    return result;
+}
+Console.WriteLine("Введите количество элементов массива: ");
+int ReadM(string m)
+{
+    int n;
+    while (true)
+        { 
+        if (int.TryParse(Console.ReadLine(), out n))
+        {
+            return n;
+        }
+        else
+        {
+            Console.WriteLine("Это не число, попробуйте еще раз");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
+int m = ReadM(Console.ReadLine());
+Console.WriteLine("Введите по очереди элементы массива: ");
+int[] array = GenerateArr(m);
+PrintArr(array);
+int res = CountNum(array);
+Console.WriteLine($"Сумма чисел больше нуля равна {res}");
